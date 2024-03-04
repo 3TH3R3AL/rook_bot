dbe6f2644ab71d42cb437112ab5b49615e671bb7
3TH3R3AL <etherealfire42@gmail.com>
2024-03-04 12:03:03 -0500

    almost finished moves, trying extracting castle flags

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
+use std::convert::From;
 use std::{fmt, usize};
 use Color::*;
 use PieceType::*;
 
 #[allow(dead_code)]
-#[derive(PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Color {
     White,
     Black,
 }
 const BOTTOM_SIDE: Color = White;
 #[allow(dead_code)]
-#[derive(PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum PieceType {
     Pawn,
     Knight,
@@ -20,11 +21,12 @@ enum PieceType {
     Queen,
     Empty,
 }
-#[derive(Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct CoordinateSet {
     x: i32,
     y: i32,
 }
+#[derive(Debug)]
 struct Direction {
     x: i32,
     y: i32,
@@ -46,10 +48,31 @@ impl std::ops::Add<&Direction> for CoordinateSet {
     }
 }
 
+impl std::ops::Add<Direction> for CoordinateSet {
+    type Output = CoordinateSet;
+
+    fn add(self, other: Direction) -> CoordinateSet {
+        CoordinateSet {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
 impl std::ops::Add<&Direction> for &CoordinateSet {
     type Output = CoordinateSet;
 
     fn add(self, other: &Direction) -> CoordinateSet {
+        CoordinateSet {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl std::ops::Add<Direction> for &CoordinateSet {
+    type Output = CoordinateSet;
+
+    fn add(self, other: Direction) -> CoordinateSet {
         CoordinateSet {
             x: self.x + other.x,
             y: self.y + other.y,
@@ -66,10 +89,8 @@ impl std::ops::Mul<i32> for &Direction {
     }
 }
 
-use Color::*;
 use MoveType::*;
-use PieceType::*;
-#[derive(Clone, Copy)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 struct Piece {
     color: Color,
     piece_type: PieceType,
@@ -80,7 +101,7 @@ struct BoardPosition {
     en_passante: Option<CoordinateSet>,
     children: Vec<BoardPosition>,
 }
-#[derive(PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 enum MoveType {
     Standard,
     CaptureOnly,
@@ -91,6 +112,7 @@ enum MoveType {
     EnPassante,
     Castle,
 }
+#[derive(Debug)]
 struct ChessMove(MoveType, Direction);
 impl Piece {
     fn new(color: Color, piece_type: PieceType) -> Piece {
@@ -116,27 +138,80 @@ impl Piece {
     fn is_empty(&self) -> bool {
         self.piece_type == Empty
     }
-    fn forward(&self) -> Direction {
+    fn forward(&self) -> i32 {
         if self.color == BOTTOM_SIDE {
-            Direction { x: 0, y: -1 }
+            -1
         } else {
-            Direction { x: 0, y: 1 }
+            1
         }
     }
     fn get_moves(&self) -> Vec<ChessMove> {
-        // This is written with the assumption that +y = forward and -y = backward, -x is queen
-        // side, +x is king side
+        // This is written with the assumption that -x is queen side, +x is king side
         match &self.piece_type {
             Pawn => vec![
-                ChessMove(NoCapture, Direction { x: 0, y: 1 }),
-                ChessMove(PawnFirst, Direction { x: 0, y: 2 }),
-                ChessMove(CaptureOnly, Direction { x: 1, y: 1 }),
-                ChessMove(CaptureOnly, Direction { x: -1, y: 1 }),
-                ChessMove(EnPassante, Direction { x: 1, y: 1 }),
-                ChessMove(EnPassante, Direction { x: -1, y: 1 }),
-                ChessMove(Promotion, Direction { x: 0, y: 1 }),
-                ChessMove(Promotion, Direction { x: 1, y: 1 }),
-                ChessMove(Promotion, Direction { x: -1, y: 1 }),
+                ChessMove(
+                    NoCapture,
+                    Direction {
+                        x: 0,
+                        y: 1 * self.forward(),
+                    },
+                ),
+                ChessMove(
+                    PawnFirst,
+                    Direction {
+                        x: 0,
+                        y: 2 * self.forward(),
+                    },
+                ),
+                ChessMove(
+                    CaptureOnly,
+                    Direction {
+                        x: 1,
+                        y: 1 * self.forward(),
+                    },
+                ),
+                ChessMove(
+                    CaptureOnly,
+                    Direction {
+                        x: -1,
+                        y: 1 * self.forward(),
+                    },
+                ),
+                ChessMove(
+                    EnPassante,
+                    Direction {
+                        x: 1,
+                        y: 1 * self.forward(),
+                    },
+                ),
+                ChessMove(
+                    EnPassante,
+                    Direction {
+                        x: -1,
+                        y: 1 * self.forward(),
+                    },
+                ),
+                ChessMove(
+                    Promotion,
+                    Direction {
+                        x: 0,
+                        y: 1 * self.forward(),
+                    },
+                ),
+                ChessMove(
+                    Promotion,
+                    Direction {
+                        x: 1,
+                        y: 1 * self.forward(),
+                    },
+                ),
+                ChessMove(
+                    Promotion,
+                    Direction {
+                        x: -1,
+                        y: 1 * self.forward(),
+                    },
+                ),
             ],
             Knight => vec![
                 ChessMove(Standard, Direction { x: -1, y: 2 }),
@@ -204,15 +279,21 @@ impl fmt::Display for Piece {
         write!(f, "{}", self.character())
     }
 }
+impl From<[[(Color, PieceType); 8]; 8]> for BoardPosition {
+    fn from(item: [[(Color, PieceType); 8]; 8]) -> BoardPosition {
+        BoardPosition {
+            board: item.map(|row| row.map(|cell| Piece::new(cell.0, cell.1))),
+            en_passante: None,
+            children: Vec::new(),
+        }
+    }
+}
 
 impl BoardPosition {
-    fn new(
-        init_arr: [[(Color, PieceType); 8]; 8],
-        en_passante: Option<CoordinateSet>,
-    ) -> BoardPosition {
+    fn new(board: [[Piece; 8]; 8]) -> BoardPosition {
         BoardPosition {
-            board: init_arr.map(|row| row.map(|cell| Piece::new(cell.0, cell.1))),
-            en_passante,
+            board,
+            en_passante: None,
             children: Vec::new(),
         }
     }
@@ -229,57 +310,44 @@ impl BoardPosition {
         self.set_piece(square, Piece::new(White, Empty));
     }
 
-    fn move_arbitrary(
-        &self,
-        start: &CoordinateSet,
-        end: &CoordinateSet,
-        cant_capture: bool,
-    ) -> Option<BoardPosition> {
+    fn move_arbitrary(&self, start: &CoordinateSet, end: &CoordinateSet) -> BoardPosition {
         debug_assert!(
             !start.out_of_bounds(),
             "start in move_arbitrary out of bounds"
         );
         debug_assert!(!end.out_of_bounds(), "end in move_arbitrary out of bounds");
-        let target = self.get_piece(end);
-        if target.piece_type != Empty {
-            let moving = self.get_piece(start);
-            if cant_capture || moving.color == target.color {
-                return None;
-            }
-        }
-
-        let mut new_board = BoardPosition::new(self.board.clone(), false);
+        let mut new_board = BoardPosition::new(self.board.clone());
         new_board.set_piece(end, *self.get_piece(start));
         new_board.clear_square(start);
-        Some(new_board)
+        new_board
     }
 
-    fn move_repeat(&mut self, start: CoordinateSet, direction: Direction, repeat: i32) {
-        let direction = &direction * repeat;
-        let target = &start + &direction;
-        if self.get_piece(&target).piece_type != Empty || target.out_of_bounds() {
+    fn move_repeat(&mut self, start: &CoordinateSet, direction: &Direction, repeat: i32) {
+        let piece = self.get_piece(start);
+        let destination = start + direction * repeat;
+        let target = self.get_piece(&destination);
+        if (!target.is_empty() && target.color == piece.color) || destination.out_of_bounds() {
             return;
         }
-        let new = self.move_arbitrary(&start, &target, false);
-        match new {
-            None => (),
-            Some(to_add) => {
-                self.children.push(to_add);
-                self.move_repeat(start, direction, repeat + 1)
-            }
+        let to_add = self.move_arbitrary(&start, &destination);
+        self.children.push(to_add);
+        if self.get_piece(&destination).piece_type != Empty {
+            return;
         }
+        self.move_repeat(start, &direction, repeat + 1)
     }
 
-    fn push_if_exists(&mut self, to_add: Option<BoardPosition>) {
-
-        match to_add {
-            None => (),
-            Some(position) => {
-                self.children.push(position);
-            }
-        }
-    }
-    fn eval_move(&mut self, piece: Piece, coords: &CoordinateSet, move_to_eval: ChessMove) {
+    // fn push_if_exists(&mut self, to_add: Option<BoardPosition>) {
+    //     match to_add {
+    //         None => (),
+    //         Some(position) => {
+    //             self.children.push(position);
+    //         }
+    //     }
+    // }
+    fn eval_move(&mut self, coords: &CoordinateSet, move_to_eval: &ChessMove) {
+        // dbg!(move_to_eval);
+        let piece = self.get_piece(coords);
         let destination = coords + &move_to_eval.1;
         if destination.out_of_bounds() {
             return;
@@ -290,30 +358,70 @@ impl BoardPosition {
                 return;
             }
         } else {
-            if move_to_eval.0 == NoCapture {
+            if let NoCapture | PawnFirst = move_to_eval.0 {
                 return;
             }
-            if move_to_eval.0 == Standard
-                || move_to_eval.0 == CaptureOnly && target.color == piece.color
-            {
+            if target.color == piece.color {
                 return;
             }
         }
+        // Guarantees going into match: destination is in bounds and capture rules are checked
         match move_to_eval.0 {
             EnPassante => match &self.en_passante {
-                None => return,
+                None => (),
                 Some(enp_coords) => {
-                    if *enp_coords != destination {
-                        return;
+                    let mut target_pawn = destination.clone();
+                    target_pawn.y -= piece.forward();
+                    if *enp_coords != target_pawn {
+                        ()
                     }
+                    debug_assert!(
+                        self.get_piece(&destination).is_empty(),
+                        "BAD En Passante Target Is Not Empty at {:?}",
+                        enp_coords
+                    );
+                    debug_assert!(
+                        *self.get_piece(&target_pawn)
+                            != Piece {
+                                piece_type: Pawn,
+                                color: piece.color
+                            },
+                        "BAD En Passante Coords Do Not Point To Proper Piece, Point To {:?}",
+                        *self.get_piece(&target_pawn)
+                    );
+                    let mut board = self.move_arbitrary(coords, &destination);
+                    board.set_piece(coords, *piece);
+                    board.clear_square(&target_pawn);
+                    board.clear_square(&coords);
                 }
             },
             PawnFirst => {
-                if
+                let row = match piece.color == BOTTOM_SIDE {
+                    true => 6,
+                    false => 1,
+                };
+                if destination.y != row {
+                    return;
+                } // In correct row
+                if !self
+                    .get_piece(&CoordinateSet {
+                        x: destination.x,
+                        y: destination.y - piece.forward(),
+                    })
+                    .is_empty()
+                {
+                    return;
+                } // Not moving through other piece
+                let mut board = self.move_arbitrary(coords, &destination);
+                board.en_passante = Some(destination);
+                self.children.push(board);
             }
-            Standard => self.push_if_exists(self.move_arbitrary(coords, &destination, false)),
-            _ => {}
-        };
+            Standard | CaptureOnly | NoCapture => self
+                .children
+                .push(self.move_arbitrary(coords, &destination)),
+            Repeat => self.move_repeat(coords, &move_to_eval.1, 1),
+            Castle => {}
+        }
     }
     // fn possible_moves(
     //     position: &[[Piece; 8]; 8],
@@ -327,21 +435,23 @@ impl BoardPosition {
     //         _ => Vec::new(),
     //     }
     // }
-    fn eval_moves(&mut self) {
-        let mut moves = Vec::new();
+    fn eval_moves(&mut self, player_color: Color) {
         for i in 0..self.board.len() {
             for j in 0..self.board[i].len() {
-                // self.eval_moves()
-                //     self.board,
-                //     CoordinateSet {
-                //         x: j as i32,
-                //         y: i as i32,
-                //     },
-                //     self.children,
-                // );
+                let target = CoordinateSet {
+                    x: j as i32,
+                    y: i as i32,
+                };
+                let piece = self.get_piece(&target);
+                if piece.color != player_color || piece.piece_type == Empty {
+                    continue;
+                }
+                self.board[i][j]
+                    .get_moves()
+                    .iter()
+                    .for_each(|move_to_eval| self.eval_move(&target, &move_to_eval));
             }
         }
-        self.children = moves;
     }
 }
 
@@ -396,19 +506,18 @@ const INITIAL_BOARD: [[(Color,PieceType); 8]; 8] = [
     [(White,Rook { has_moved: false }),(White,Knight),(White,Bishop),(White,Queen),(White,King { has_moved: false }),(White,Bishop),(White,Knight),(White,Rook { has_moved: false })],
 ];
 fn main() {
-    let piece = Piece::new(White, Pawn);
     let mut init_position = match BOTTOM_SIDE {
-        White => BoardPosition::new(INITIAL_BOARD, None),
+        White => BoardPosition::from(INITIAL_BOARD),
         Black => {
             let mut half_reverse = INITIAL_BOARD.map(|mut row| {
                 row.reverse();
                 row
             });
             half_reverse.reverse();
-            BoardPosition::new(half_reverse, None)
+            BoardPosition::from(half_reverse)
         }
     };
-    init_position.eval_moves();
+    init_position.eval_moves(White);
     for position in init_position.children {
         println!("{}", position);
     }
